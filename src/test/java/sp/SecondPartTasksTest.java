725adac5e54e5e2701a7e494530e51997f146d2d
gutsolksenia <tatsigvaris@mail.ru>
2016-02-23 00:26:07 +0400

    added tests to calculateGlobalOrder

--- src/test/java/sp/SecondPartTasksTest.java
+++ src/test/java/sp/SecondPartTasksTest.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static sp.SecondPartTasks.calculateGlobalOrder;
 import static sp.SecondPartTasks.findQuotes;
 
 public class SecondPartTasksTest {
@@ -35,6 +38,44 @@ public class SecondPartTasksTest {
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        Map<String, Integer> ord1 = new HashMap<String, Integer>();
+        ord1.put("pizza", 10);
+        ord1.put("banana", 100);
+        ord1.put("cola", 20);
+
+        Map<String, Integer> ord2 = new HashMap<String, Integer>();
+        ord2.put("pizza", 30);
+        ord2.put("banana", 10);
+        ord2.put("fanta", 20);
+
+        Map<String, Integer> ord3 = new HashMap<String, Integer>();
+        ord3.put("pizza", 2);
+        ord3.put("apple", 10);
+        ord3.put("banana", 20);
+
+        Map<String, Integer> answ1 = new HashMap<String, Integer>();
+        answ1.put("pizza", 42);
+        answ1.put("banana", 130 );
+        answ1.put("cola", 20);
+        answ1.put("fanta", 20);
+        answ1.put("apple", 10);
+
+        assertEquals(calculateGlobalOrder(Arrays.asList(ord1, ord2, ord3)), answ1);
+
+        Map<String, Integer> answ2 = new HashMap<String, Integer>();
+        answ2.put("pizza", 40);
+        answ2.put("banana", 110);
+        answ2.put("cola", 20);
+        answ2.put("fanta", 20);
+
+        assertEquals(calculateGlobalOrder(Arrays.asList(ord1, ord2)), answ2);
+
+        Map<String, Integer> answ3 = new HashMap<String, Integer>();
+        answ3.put("pizza", 32);
+        answ3.put("banana", 30);
+        answ3.put("apple", 10);
+        answ3.put("fanta", 20);
+
+        assertEquals(calculateGlobalOrder(Arrays.asList(ord2, ord3)), answ3);
     }
 }
